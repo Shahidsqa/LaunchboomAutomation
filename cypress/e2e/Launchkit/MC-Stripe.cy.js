5b15f1386d42f35585754762bb151e6d27b17b8c
Shahid <141882126+Shahidsqa@users.noreply.github.com>
2024-12-30 23:34:04 +0500

    Update MC-Stripe.cy.js

--- cypress/e2e/Launchkit/MC-Stripe.cy.js
+++ cypress/e2e/Launchkit/MC-Stripe.cy.js
@@ -4,7 +4,7 @@ import McStripeOauth from "../../PageObjectModel/MC-StripeOauthClass"
 ///<reference types = "cypress"/>
 // returning false here prevents cypress from failing tests
 Cypress.on("uncaught:exception",()=>false)
-it('Mailchimp Oauth',function(){
+it('Mailchimp/Stripe Oauth',function(){
     cy.viewport(1200,600)
     const obj2 = new AiContent()
     const obj1 = new McStripeOauth()
