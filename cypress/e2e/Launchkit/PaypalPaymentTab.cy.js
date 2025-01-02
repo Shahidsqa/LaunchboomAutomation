2d884e4058288462085f0b9c51c6c19e8a070c3b
Shahid <141882126+Shahidsqa@users.noreply.github.com>
2025-01-03 00:34:41 +0500

    Add files via upload

---
+++ cypress/e2e/Launchkit/PaypalPaymentTab.cy.js
@@ -0,0 +1,26 @@
+import paypalpaymentTab from "../../PageObjectModel/PaypalPaymentTabClass"
+import AiContent from "../../PageObjectModel/Ai-content-Class"
+// enable intelligence
+///<reference types = "cypress"/>
+// returning false here prevents cypress from failing tests
+Cypress.on("uncaught:exception",()=>false)
+
+it('Paypal Payment Tab Testing',function(){
+    cy.viewport(1200,600)
+    const obj2 = new AiContent()
+    const obj1 = new paypalpaymentTab()
+    obj2.Login()
+    obj1.SelectPaymentMethod()
+    obj1.MakeAllTogglesOff()
+    obj1.Currencies()
+    obj1.CheckoutwithReservationCost()
+    obj1.CheckoutwithPhoneNumber()
+    obj1.CheckoutwithBillingAddress()
+    obj1.DisableAdvancePaypal()
+    obj1.MakeAllTogglesOff()
+    obj1.CheckoutwithOldPaypal()
+     
+    
+    
+
+})
