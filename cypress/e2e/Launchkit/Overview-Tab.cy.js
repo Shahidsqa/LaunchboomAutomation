18f61b6020cf6a37dc8fb5d6895a6825a773591e
Shahid <141882126+Shahidsqa@users.noreply.github.com>
2025-01-28 00:00:39 +0500

    Update Overview-Tab.cy.js

--- cypress/e2e/Launchkit/Overview-Tab.cy.js
+++ cypress/e2e/Launchkit/Overview-Tab.cy.js
@@ -1,7 +1,6 @@
 import OverviewTab from "../../PageObjectModel/OverviewTabClass"
 import AiContent from "../../PageObjectModel/Ai-content-Class"
 import funnelTypes from "../../PageObjectModel/FunnelClass"
-import stripepaymentTab from "../../PageObjectModel/StripePaymentTabClass"
 // enable intelligence
 ///<reference types = "cypress"/>
 // returning false here prevents cypress from failing tests
@@ -11,7 +10,6 @@ it('Overview Tab Testing',function(){
     const obj1 = new OverviewTab()
     const obj2 = new AiContent()
     const obj3 = new funnelTypes()
-    const obj4 = new stripepaymentTab()
     obj2.Login()
     obj2.CreateProject()
     obj1.UpdateProjectName()
@@ -21,9 +19,8 @@ it('Overview Tab Testing',function(){
     obj1.Redirection()
     obj1.RedirectionToggle()
     obj1.FontsSelection()
-    obj4.SelectPaymentMethod()
-    obj4.MakeAllTogglesOff()
-    obj1.SaveSettings()
+    obj3.StripeMethod()
+    obj3.BillingAddress()
     obj3.optoutFunnel()
     obj1.OptoutFlowFontsChecking()
     obj3.followerFunnel()
